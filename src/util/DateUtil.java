29471bfb1387dd136b479db94db0e0273c758b87
mac <shihao@tabcut.com>
2022-12-29 15:45:46 +0800

    sameDayCompare相同日期的时间比较

--- src/util/DateUtil.java
+++ src/util/DateUtil.java
@@ -1,4 +1,16 @@
 package util;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 public class DateUtil {
+        public static Boolean sameDayCompare(String dateOne, String dateTwo){
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+            try {
+                if (simpleDateFormat.parse(dateOne).getTime() > simpleDateFormat.parse(dateTwo).getTime()){
+                    return true;
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
 }
